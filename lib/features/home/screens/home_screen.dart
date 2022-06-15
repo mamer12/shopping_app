d91966b62b7afeae9ae1006e8870a3692cfdd8ab
mamer12 <46693079+mamer12@users.noreply.github.com>
2022-06-15 10:18:18 +0300

    KEEPITGOING

---
+++ lib/features/home/screens/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/features/widgets/address_box.dart';
+import 'package:shopping_app/provieders/user_provider.dart';
+
+import '../../../constatns/global_var.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "/home";
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: AppBar(
+              elevation: 0,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    gradient: GlobalVariables.appBarGradient),
+              ),
+              title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                        child: Container(
+                            height: 42,
+                            margin: const EdgeInsets.only(left: 15),
+                            child: Material(
+                                borderRadius: BorderRadius.circular(7),
+                                elevation: 1,
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                      prefixIcon: InkWell(
+                                          onTap: () {},
+                                          child: const Padding(
+                                            padding: EdgeInsets.only(left: 6),
+                                            child: Icon(Icons.search_outlined,
+                                                color: Colors.black, size: 23),
+                                          )),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      contentPadding:
+                                          const EdgeInsets.only(top: 10),
+                                      border: const OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(7)),
+                                        borderSide: BorderSide.none,
+                                      ),
+                                      enabledBorder: const OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(7)),
+                                        borderSide: BorderSide(
+                                            color: Colors.black, width: 1),
+                                      ),
+                                      hintText: "Search in the app",
+                                      hintStyle: const TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 17)),
+                                )))),
+                    Container(
+                        color: Colors.transparent,
+                        height: 42,
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: const Icon(
+                          Icons.mic_outlined,
+                          color: Colors.white,
+                        ))
+                  ]),
+            )),
+        body: Column(
+          children: const [AddressBox()],
+        ));
+  }
+}
